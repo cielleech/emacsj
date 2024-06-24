726748613799f071abb77d0876ada26604c45326
Nobita.lee <cielleech@outlook.com>
2024-06-24 14:32:43 +0800

    fix bug when kill line

--- src/main/kotlin/com/github/strindberg/emacsj/kill/KillHandler.kt
+++ src/main/kotlin/com/github/strindberg/emacsj/kill/KillHandler.kt
@@ -17,15 +17,24 @@ class KillHandler : EditorWriteActionHandler.ForEachCaret() {
 
     private fun getEndOffset(offset: Int, document: Document): Int {
         val endOffset = DocumentUtil.getLineEndOffset(offset, document)
+
         return minOf(
-            document.textLength - 1,
+            document.textLength,
             if (CharArrayUtil.isEmptyOrSpaces(document.charsSequence, offset, endOffset)) {
-                endOffset + 1
-            } else if (DocumentUtil.isAtLineStart(offset, document)) {
                 endOffset + 1
             } else {
                 endOffset
             }
         )
+//        return minOf(
+//            document.textLength - 1,
+//            if (CharArrayUtil.isEmptyOrSpaces(document.charsSequence, offset, endOffset)) {
+//                endOffset + 1
+////            } else if (DocumentUtil.isAtLineStart(offset, document)) {
+////                endOffset + 1
+//            } else {
+//                endOffset
+//            }
+//        )
     }
 }
